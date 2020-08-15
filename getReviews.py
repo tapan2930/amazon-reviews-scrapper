84128b635675e63d2dbdd3f605b0141a442cd4e9
Tapan Patel <tapan2930@gmail.com>
2020-08-15 22:28:23 +0530

    changed scraping reviews of only verified amazon user

--- getReviews.py
+++ getReviews.py
@@ -9,8 +9,19 @@ total_page = 5
 data_df = pd.DataFrame(columns=["Reviews","Date","Rating"])
 name_sufix= 1
 
+def check_url(url: str):
+    if url == None:
+        return None
+    return 1    
+
+
 def process_url(url: str):
     url = re.search(r".+(\/dp\/).+(\/)", url)
+    status = check_url(url)
+    if status == None:
+        print("Please enter the proper Url or try again later")
+        print("Exiting the application....")
+        exit()
     url = url.group(0)
     url = re.sub(r"(\/dp\/)","/product-reviews/", url)
     return url
@@ -57,7 +68,7 @@ def scrape(url:str):
     stars = ["five_star","four_star","three_star","two_star","one_star"]
 
     for i in range(total_page):
-        adder = f"ref=cm_cr_getr_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&filterByStar={stars[i]}&pageNumber={1}"
+        adder = f"ref=cm_cr_getr_d_paging_btm_next_2?ie=UTF8&reviewerType=avp_only_reviews&filterByStar={stars[i]}&pageNumber={1}"
         link = url+adder
         res = get_request(link)
         soup = bs4.BeautifulSoup(res.text,"lxml")
